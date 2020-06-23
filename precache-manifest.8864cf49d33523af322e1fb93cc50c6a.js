e894b8e7abba6145923a415c9072b9b2527576e5
ohyeyoye <33212292+ohyeyoye@users.noreply.github.com>
2020-06-23 14:42:38 +0900

    Updates

---
+++ precache-manifest.8864cf49d33523af322e1fb93cc50c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56b39b5a02a033cdd3571e549420959",
+    "url": "/tldr/index.html"
+  },
+  {
+    "revision": "7072a512c5de1838441e",
+    "url": "/tldr/static/css/main.862931fc.chunk.css"
+  },
+  {
+    "revision": "2c820745ce267b2a24dc",
+    "url": "/tldr/static/js/2.ea4d3644.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/tldr/static/js/2.ea4d3644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7072a512c5de1838441e",
+    "url": "/tldr/static/js/main.e98b59a3.chunk.js"
+  },
+  {
+    "revision": "1fc5703ad76f3e0de6df",
+    "url": "/tldr/static/js/runtime-main.e4a33ac6.js"
+  }
+]);
